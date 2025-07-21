73e6b08903a9e5391adf045d6a437bb5475281fc
AD221606 <aryandubey221606@gmail.com>
2025-07-21 20:31:57 +0530

    hace to shift to a Gameboard component

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,7 @@ function App() {
           <Player name="Player 2" playerSymbol="O" />
         </div>
 
-        <div id="game-area">
-          <div className="game-box">Box 1</div>
-          <div className="game-box">Box 2</div>
-          <div className="game-box">Box 3</div>
-          <div className="game-box">Box 4</div>
-          <div className="game-box">Box 5</div>
-          <div className="game-box">Box 6 </div>
-          <div className="game-box">Box 7</div>
-          <div className="game-box">Box 8</div>
-          <div className="game-box">Box 9</div>
-        </div>
+        
       </main>
     </>
   );
