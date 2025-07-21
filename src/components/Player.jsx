cdd4251d2a7aa08b4991568400a31b7f8eab791f
AD221606 <aryandubey221606@gmail.com>
2025-07-21 14:30:54 +0530

    Finished Player Name Input logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,15 +2,23 @@ import { useState } from "react";
 
 export default function Player({name}){
 
+    const [isEditing, setIsEditing] = useState(false);
+    const editStatus = isEditing? "Save": "Edit";
     const [playerName, setPlayerName] = useState(name);
 
     const handlePlayerChange = (event) => {
         const newPlayer = event.target.value;
-        setPlayerName((oldPlayer)=> newPlayer);
+        setPlayerName(newPlayer);
     }
+
+    const toggleEdit = ()=>{
+        setIsEditing((prevStatus)=>!prevStatus);
+    }
+
     return(
         <>
-            <input type="text" required value={playerName} onChange={handlePlayerChange}/>
+            {isEditing?<input className="playerInput" type="text" required value={playerName} onChange={handlePlayerChange}/>:<input readOnly className="playerInput" value={playerName}/>}
+            <button onClick={toggleEdit}>{editStatus}</button>
         </>
     )
 }
